ff9c070b5f335f4d22611757f446677696b20982
Ejemplo <56851834+JhonnyPascual@users.noreply.github.com>
2019-12-02 20:10:03 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Datos',
+      home: MyCustomForm()
+    );
+  }
+}
+
+// Define un widget de formulario personalizado
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define una clase de estado correspondiente. Esta clase contendrá los datos relacionados
+// con nuestro formulario.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Crea un controlador de texto. Lo usaremos para recuperar el valor actual
+  // del TextField!
+  final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    // Limpia el controlador cuando el widget se elimine del árbol de widgets
+    // Esto también elimina el listener _printLatestValue
+    myController.dispose();
+    super.dispose();
+  }
+
+  _printLatestValue() {
+    print("Second text field: ${myController.text}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (text) {
+                print("First text field: $text");
+              },
+            ),
+            TextField(
+              controller: myController,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
